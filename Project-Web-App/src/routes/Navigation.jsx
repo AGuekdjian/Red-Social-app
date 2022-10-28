35e468091500f45fc539776bfd23772af22203f9
Anthony Guekdjian <anthonyguekdjian@gmail.com>
2022-10-27 22:11:39 -0300

    modificaciones react router

--- Project-Web-App/src/routes/Navigation.jsx
+++ Project-Web-App/src/routes/Navigation.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import routes from "./routes";
+
+// Pages
 import Home from "../pages/Home"
 import User from "../pages/User"
 import Error404 from "../pages/Error404"
+
+// Layouts
+import LayoutBasic from "../layouts/LayoutBasic";
 
 export default function Navigation() {
     return (
@@ -15,7 +21,7 @@ export default function Navigation() {
                 />
 
                 <Route
-                    path='/user'
+                    path='/:username'
                     element={<User />}
                     exact={true}
                 />
